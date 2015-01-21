7759f678e5d6c63f957233045dea6a1e644c8709
Eugene Sharygin <eush77@gmail.com>
2015-01-21 13:45:41 +0300

    Add usage.txt and --help

--- cli.js
+++ cli.js
@@ -5,14 +5,29 @@ var Styler = require('./src/ass-styler');
 
 var concat = require('concat-stream');
 
+var fs = require('fs');
 
-process.stdin.pipe(concat({ encoding: 'string' }, function (text) {
-  var styler = Styler(text);
 
-  process.argv.slice(2).forEach(function (expr) {
-    var m = expr.match(/^([^.]+)\.([^.]+)=(.*)$/);
-    styler.set(m[1], m[2], m[3]);
-  });
+var argv = process.argv.slice(2);
 
-  console.log(styler.text());
-}));
+
+var usage = function () {
+  fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
+};
+
+
+var main = function () {
+  process.stdin.pipe(concat({ encoding: 'string' }, function (text) {
+    var styler = Styler(text);
+
+    argv.forEach(function (expr) {
+      var m = expr.match(/^([^.]+)\.([^.]+)=(.*)$/);
+      styler.set(m[1], m[2], m[3]);
+    });
+
+    console.log(styler.text());
+  }));
+};
+
+
+(argv.length == 1 && argv == '--help') ? usage() : main();
